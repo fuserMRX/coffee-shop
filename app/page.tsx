8d63dc4e632408f2471ac6414d1d66e63359a9e8
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-02-27 11:56:14 +0200

    fix: fox image component for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,56 @@
 import Banner from "@/components/banner.client";
 import Card from "@/components/card.server";
+import { CoffeeStoreType } from "@/types";
 
 export default function Home() {
   const coffeeStoreId = 'dark-horse-coffee';
+  const coffeeStores = [
+    {
+      "name": "StrangeLove Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1511920170033-f8396924c348?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "Dark horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "StrangeLove Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1511920170033-f8396924c348?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "Dark horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1447933601403-0c6688de566e?q=80&w=2561&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "StrangeLove Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1447933601403-0c6688de566e?q=80&w=2561&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "Dark horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1518057111178-44a106bad636?q=80&w=2136&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-14">
-      <Banner />
-      <Card
-        name='Dark horse coffee'
-        imgUrl='/static/hero-image.png'
-        href={`coffee-store/${coffeeStoreId}`}
-      />
-    </main>
+    <div className="mb-56">
+      <main className="mx-auto mt-10 max-w-6xl px-4">
+        <Banner />
+        <div className="mt-20">
+          <h2 className="mt-8 pb-8 text-4xl font-bold text-white">
+            Toronto Stores
+          </h2>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
+            {coffeeStores.map((coffeeStore: CoffeeStoreType, idx: number) => (
+              <Card
+                key={`${coffeeStore.name}-${idx}`}
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${idx}`}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
---
+++ types/index.ts
@@ -0,0 +1,21 @@
+export type CoffeeStoreType = {
+    // id: string;
+    name: string;
+    imgUrl: string;
+    // address: string;
+    // voting: number;
+};
+
+export type MapboxType = {
+    id: string;
+    properties: {
+        address: string;
+    };
+    text: string;
+};
+
+export type AirtableRecordType = {
+    id: string;
+    recordId: string;
+    fields: CoffeeStoreType;
+};
