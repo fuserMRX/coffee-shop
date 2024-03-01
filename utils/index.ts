55541728ee78c0c3ce0a5f2490d6329976f0669a
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-03-01 18:50:45 +0200

    feat: add another domain name for a prod app

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 export const getDomain = () => {
     return new URL(
         process.env.NODE_ENV === 'production'
-            ? 'https://discover-coffee-stores-latest.vercel.app'
+            ? 'https://coffee-shop-chi-drab.vercel.app/'
             : 'http://localhost:3000'
     )
 }
